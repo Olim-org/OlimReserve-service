01f57d96d8022db1714abaf7f3c7a78d62848e9d
wodn1478 <wodn14789@naver.com>
2024-02-15 01:39:35 +0900

    fix : Update Feign client param #18
    센터 정보를 가져오기 위한 Feign 클라이언트 정의를 수정함.
    
    Github issue #18

--- src/main/java/com/olim/reserveservice/client/CustomerClient.java
+++ src/main/java/com/olim/reserveservice/client/CustomerClient.java
@@ -4,9 +4,11 @@ import com.olim.reserveservice.dto.response.CenterFeignResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @FeignClient(value = "customer-service", path = "/customer-service")
 public interface CustomerClient {
     String AUTHORIZATION = "Authorization";
     @GetMapping("/center/info")
-    CenterFeignResponse getCenterInfo(@RequestHeader(AUTHORIZATION) String token, String centerId);
+    CenterFeignResponse getCenterInfo(@RequestHeader(AUTHORIZATION) String token, @RequestParam(value = "centerId") String centerId);
 }
