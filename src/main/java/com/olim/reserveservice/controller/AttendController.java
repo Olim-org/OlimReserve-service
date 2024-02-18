2df81bfac748d259f5dfcc74f90e78618771d938
wodn1478 <wodn14789@naver.com>
2024-02-18 18:57:44 +0900

    feat : Add @Tag, @Validated annotation #46
    Swagger 제목, 설명 추가
    Validated로 데이터 검열
    
    Github issue #46

--- src/main/java/com/olim/reserveservice/controller/AttendController.java
+++ src/main/java/com/olim/reserveservice/controller/AttendController.java
@@ -9,10 +9,12 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -21,6 +23,8 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/reserve-service/attend")
+@Tag(name = "출석 체크", description = "출석 체크 관련 API 구성")
+@Validated
 public class AttendController {
 private final AttendService attendService;
     private final CustomerClient customerClient;
