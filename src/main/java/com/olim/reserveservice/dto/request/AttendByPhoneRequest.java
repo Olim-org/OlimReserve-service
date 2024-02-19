6989a079eb8b3334dd1b7bf0ca93c51e3322a4d6
wodn1478 <wodn14789@naver.com>
2024-02-19 21:35:55 +0900

    fix : Update regexp of phoneNumber #75
    휴대폰 번호의 정규식을 4자리로 변경
    
    Github issue #75

--- src/main/java/com/olim/reserveservice/dto/request/AttendByPhoneRequest.java
+++ src/main/java/com/olim/reserveservice/dto/request/AttendByPhoneRequest.java
@@ -12,7 +12,7 @@ public record AttendByPhoneRequest(
         UUID centerId,
         @Schema(description = "전화번호", example = "01012345678")
         @NotBlank
-        @Pattern(regexp = "^\\d{3}\\d{3,4}\\d{4}$", message = "전화번호 형식이 올바르지 않습니다.")
+        @Pattern(regexp = "^\\d{4}$", message = "전화번호 형식이 올바르지 않습니다.")
         String phoneNumber
 ) {
 }
